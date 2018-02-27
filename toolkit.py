e0df2cec2b0b95db62741a02abd206fe4e5882dc
Eric Davis <mojo@linuxbookpro.attlocal.net>
2018-02-26 23:59:09 -0600

    get account balances, next put in dataframe

--- toolkit.py
+++ toolkit.py
@@ -1,5 +1,7 @@
 from client import BinanceRESTAPI, BinanceWebSocketAPI
 import config
+import json
+import pandas as pd
 
 rest_client = BinanceRESTAPI(config.KEY, config.SECRET)
 ws_client = BinanceWebSocketAPI(config.KEY)
@@ -10,9 +12,14 @@ def ping():
 def last_day_market_stats():
     prices = rest_client.all_prices()
     for price in prices:
-        print price.symbol, price.price
+        return price.symbol, price.price
 
 def my_trade_by_pair(pair):
     trades = rest_client.my_trades("{0}".format(pair))
     for trade in trades:
-        print trade.id, trade.price, trade.qty
+        return trade.id, trade.price, trade.qty
+
+def account_balances():
+    account = rest_client.account()
+    for balance in account.balances:
+        print balance.asset, balance.free, balance.locked # build dateframe here to return
