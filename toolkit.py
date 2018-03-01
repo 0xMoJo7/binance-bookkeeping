aaa576fd33afd9614c85ca5b2049d6c2f8ae43eb
Eric Davis <mojo@linuxbookpro.attlocal.net>
2018-03-01 00:40:01 -0600

    fix things into dataframes, add portfolio value
    check math, portfolio feels off

--- toolkit.py
+++ toolkit.py
@@ -9,15 +9,12 @@ ws_client = BinanceWebSocketAPI(config.KEY)
 def ping():
     return rest_client.ping()
 
-def last_day_market_stats():
+def market_prices():
+    market_df = pd.DataFrame(columns=['asset', 'price'])
     prices = rest_client.all_prices()
     for price in prices:
-        print price.symbol, price.price
-
-def account_balances():
-    account = rest_client.account()
-    for balance in account.balances:
-        print balance.asset, balance.free, balance.locked
+        market_df = market_df.append({'asset' : str(price.symbol), 'price' : price.price}, ignore_index=True)
+    return market_df
 
 def my_trades_by_pair(pair):
     trade_df = pd.DataFrame(columns=['id', 'price', 'quantity'])
@@ -33,7 +30,7 @@ def account_balances_df():
     account = rest_client.account()
     for balance in account.balances:
         if float(balance.free) > 0.0 or float(balance.locked) > 0.0:
-            balances_df = balances_df.append({'asset': str(balance.asset),
+            balances_df = balances_df.append({'asset': str(balance.asset) + 'BTC',
                                               'free': float(balance.free),
                                               'locked': float(balance.locked)}, ignore_index=True)
     return balances_df
@@ -52,3 +49,18 @@ def trade_history():
         else:
             pass
     return trade_history_df
+
+def portfolio_value():
+    market_df = market_prices()
+    balances_df = account_balances_df()
+    portfolio_df = pd.merge(balances_df, market_df, on='asset', how='inner')
+    portfolio_df['usd_value'] = ''
+    btc_usd = market_df.loc[market_df['asset'] =='BTCUSDT']
+    print btc_usd
+    for kv, row in portfolio_df.iterrows():
+        total_holding = row['free'] + row['locked']
+        usd = total_holding * float(btc_usd['price'])
+        row['usd_value'] = usd
+    print portfolio_df
+
+
