be1bff094661159445d1255980721e9f22f3f9fa
Eric Davis <mojo@linuxbookpro.attlocal.net>
2018-02-28 18:52:00 -0600

    add func to fetch account balance and trade history

--- toolkit.py
+++ toolkit.py
@@ -12,9 +12,9 @@ def ping():
 def last_day_market_stats():
     prices = rest_client.all_prices()
     for price in prices:
-        return price.symbol, price.price
+        print price.symbol, price.price
 
-def my_trade_by_pair(pair):
+def my_trades_by_pair(pair):
     trades = rest_client.my_trades("{0}".format(pair))
     for trade in trades:
         return trade.id, trade.price, trade.qty
@@ -22,4 +22,24 @@ def my_trade_by_pair(pair):
 def account_balances():
     account = rest_client.account()
     for balance in account.balances:
-        print balance.asset, balance.free, balance.locked # build dateframe here to return
+        print balance.asset, balance.free, balance.locked
+
+def account_balances_df():
+    balances_df = pd.DataFrame(columns=['asset', 'free', 'locked'])
+    account = rest_client.account()
+    for balance in account.balances:
+        if float(balance.free) > 0.0 or float(balance.locked) > 0.0:
+            balances_df = balances_df.append({'asset': str(balance.asset), 'free': float(balance.free), 'locked': float(balance.locked)}, ignore_index=True)
+    return balances_df
+
+def trade_history():
+    trade_history_df = pd.DataFrame(columns=['id', 'asset', 'price', 'quantity'])
+    balances_df = account_balances_df()
+    for kv, row in balances_df.iterrows():
+        if row['asset'] != 'BTC':
+            asset_trades_df = my_trades_by_pair('{0}BTC'.format(row['asset']))
+            for kv, trade in asset_trades_df.iterrows():
+                trade_history_df.append({'id' : trade['id'], 'asset': row['asset'][0], 'price': trade['price'], 'quantity' : trade['quantity']})
+        else:
+            pass
+    return trade_history_df
